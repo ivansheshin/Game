f291ccb68883475005b71d18b724c0bf89f03ea8
ivansheshin <ivansheshin@gmail.com>
2021-06-20 18:27:26 +0300

    123

---
+++ app/js/main.js
@@ -0,0 +1,51 @@
+    const sectionBlue = document.querySelector('.game-object__section[data-id="1"');
+    const sectionYellow = document.querySelector('.game-object__section[data-id="2"');
+    const sectionGreen = document.querySelector('.game-object__section[data-id="3"');
+    const sectionRed = document.querySelector('.game-object__section[data-id="4"');
+    const button = document.querySelector('.game-information__btn');
+
+    const arraySection = [sectionBlue, sectionYellow, sectionGreen, sectionRed];
+    const random = arraySection[Math.floor(Math.random()*arraySection.length)];
+
+
+
+    const newArray = [];
+    const clickedArray = [];
+    const roundInfo = document.getElementById('round').innerHTML = `${clickedArray.length}`; 
+    sectionBlue.addEventListener ('click', function() {
+        newArray.push(sectionBlue);
+    });
+    sectionYellow.addEventListener ('click', function() {
+        newArray.push(sectionYellow);
+    });
+    sectionGreen.addEventListener ('click', function() {
+        newArray.push(sectionGreen);
+    });
+    sectionRed.addEventListener ('click', function() {
+        newArray.push(sectionRed);
+    });
+
+    button.addEventListener('click', function(){
+        addElemArr(1);
+        const roundInfo = document.getElementById('round').innerHTML = clickedArray.length; 
+        roundInfo;
+        for (let i = 0; i < clickedArray.length; i++){
+            clickedArray[i].classList.add(':hover');
+        }
+
+
+        
+})
+console.log(newArray);
+
+
+function addElemArr (n) {
+
+    for (let i = 1; i <= n; i++) {
+        
+        let x = arraySection[Math.floor(Math.random()*arraySection.length)]
+        clickedArray.push(x);
+    }
+
+    console.log(clickedArray)
+}
