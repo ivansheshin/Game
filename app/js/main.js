00415c2f0df91a999789c037834b994692b821c7
ivansheshin <ivansheshin@gmail.com>
2021-06-27 16:59:31 +0300

    123

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,13 @@
 const section = document.querySelectorAll('.game-object__section');
 const button = document.querySelector('.game-information__btn');
-let roundValue = document.getElementById('round').textContent = 0;
+let roundValue = document.getElementById('round').textContent;
 
 let randomArray = [];
 let clickedArray = [];
 
 button.addEventListener('click', function () {
-    if(randomArray.length - clickedArray.length < 0){
-        randomArray = []
-    }
-    else{
-         addElemRandom();
-    // console.log(randomArray)
+    addElemRandom();
+
     randomArray.forEach((item, i) => {
         setTimeout(() => {
             item.classList.add('animation');
@@ -19,33 +15,31 @@ button.addEventListener('click', function () {
                 item.classList.remove('animation')
             }, 500)
         }, i * 800)
-    });
-    }
-    
+    });   
+
 })
 
-    section.forEach((item) => {
+let secListener = section.forEach((item) => {
     item.addEventListener('click', function () {
         clickedArray.push(item);
-        // console.log(clickedArray)
-        // console.log(clickedArray[clickedArray.length - 1])
-        compare(randomArray, clickedArray)
+        compare(randomArray, clickedArray);
 
     })
 })
 
 
 function addElemRandom() {
-    for (let i = 0; i < 1; i++) {
-        let x = section[Math.floor(Math.random() * section.length)]
-        randomArray.push(x);
-    }
+        for (let i = 0; i < 1; i++) {
+                let x = section[Math.floor(Math.random() * section.length)]
+                randomArray.push(x);
+        }
+        
 }
 
 function compare(a, b) {
     if (a.length === b.length &&
         a.every((v, i) => v === b[i])) {
-        document.getElementById('round').textContent = clickedArray.length
+        document.getElementById('round').textContent = b.length
         clickedArray = []
         console.log(true)
     } else if (a.length > b.length) {
@@ -56,4 +50,3 @@ function compare(a, b) {
         clickedArray = []
     }
 }
-console.log(clickedArray)
